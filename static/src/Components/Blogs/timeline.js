e84c5ee2288d232f815f62bbe4596277c8599832
Utkarsh Malik <utkarshmalik06@gmail.com>
2018-07-28 18:58:33 +0530

    Timeline-issue

--- static/src/Components/Blogs/timeline.js
+++ static/src/Components/Blogs/timeline.js
@@ -2,28 +2,29 @@ import React from 'react';
 import HorizontalTimeline from 'react-horizontal-timeline';
 import './timeline.css';
 
-const VALUES = ["2013-02-12","2013-02-12","2013-02-12","2013-02-12","2013-02-12"];
+const VALUES = [ "2010-12-05","2009-06-05","1998-06-05","2001-12-08","2010-12-05","2010-12-05"];
 
-export default class Timeline extends React.Component {
+export default class App extends React.Component {
   state = { value: 0, previous: 0 };
 
   render() {
     return (
-      <div className="timeline_box">
+      <div className="timeline-box">
         {/* Bounding box for the Timeline */}
-        <div style={{ width: '60%', height: '100px', margin: '0 auto' }}>
+        <div style={{ width: '80%', height: '100px', margin: '0 auto' }}>
           <HorizontalTimeline
             index={this.state.value}
             indexClick={(index) => {
               this.setState({ value: index, previous: this.state.value });
             }}
-            values= {VALUES} />
+            values={ VALUES } />
         </div>
         <div className='text-center'>
-        <h1> Hello </h1>   
+          {/* any arbitrary component can go here */}    
           {this.state.value}
         </div>
       </div>
     );
   }
 }
+
