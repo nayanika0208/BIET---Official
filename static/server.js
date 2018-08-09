60e63dc34ef33c3292eab21694952ba50f18e195
Utkarsh Malik <utkarshmalik06@gmail.com>
2018-08-09 19:20:22 +0530

    Server added

---
+++ static/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path= require('path');
+const app= express();
+
+
+
+  app.use(express.static(path.join(__dirname,'/build')));
+
+  app.get('*',(req,res) => 
+{
+  res.sendFile(path.join(__dirname + '/build/index/html'));
+});
+
+
+const PORT = process.env.port || 3001;
+
+app.listen(PORT,()=>
+{
+  console.log('port',PORT);
+})
+
+
